ccbdd3671667221a7f3881fad49dd1dfc4aa19c7
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-04-10 10:38:21 -0300

    add component Navmenu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="nav">
-
+    <Navmenu/>
   </div>
   <router-view/>
+  
 </template>
+
+<script>
+import Navmenu from './components/Navmenu'
+
+export default{
+name:'App',
+components:{
+  Navmenu
+}
+}
+</script>
+
 
 <style>
 
---
+++ src/components/Navmenu.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a>
+        <img src="../assets/logo.png" width="200" height="100">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+        <a class="navbar-item">
+            Home
+        </a>
+
+        <a class="navbar-item">
+            Vagas
+        </a>
+
+        <a class="navbar-item">
+            Questões Anac
+        </a>
+
+        <a class="navbar-item">
+            Apostila
+        </a>
+
+        </div>
+
+        <div class="navbar-end">
+        <div class="navbar-item">
+            <div class="buttons">
+            <a class="button is-info">
+                <strong>Cadastre-se</strong>
+            </a>
+            <a class="button is-link is-light">
+                Log in
+            </a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </nav>
+
+</template>
